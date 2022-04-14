947c2113e810b4458e34105f9abadbe695ef2551
Sandra Kopeć <87870065+sandrak115@users.noreply.github.com>
2022-04-14 20:27:52 +0200

    link changed
    I've changed a link to google forms with my own

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1d32M9l3b8aCar9BSzJlSlTY5uFMTcMIc47tGE9hdziY/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
